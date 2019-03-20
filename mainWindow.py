27525f4296589a5e19eb21fcf39cca0044137733
Hansen <abrandarizdominguez@danielcastelao.org>
2019-03-20 19:45:25 +0100

    New functions added to manage database and Gtk comboBoxText

--- mainWindow.py
+++ mainWindow.py
@@ -10,8 +10,8 @@ class FiestraPrincipal ():
 
         self.controlBloqueCliente = False
         self.controlBloqueProducto = False
-        bbdd = dbapi2.connect("bbdd.dat")
-        self.cursor = bbdd.cursor()
+        self.bbdd = dbapi2.connect("bbdd.dat")
+        self.cursor = self.bbdd.cursor()
 
         builder = Gtk.Builder()
         builder.add_from_file ("mainWindow.glade")
@@ -21,6 +21,14 @@ class FiestraPrincipal ():
         # Cuando se presione el boton botonAñadir ese bloque se hará visible, se inicia como invisible:
         self.newClienteBlock = builder.get_object("bloqueNuevoCliente")
         self.newProductoBlock = builder.get_object("bloqueNuevoProducto")
+
+        # ComboBox:
+        self.comboClientes = builder.get_object("cbListaClientes")
+        clientes = self.cursor.execute("select dni, nombre from clientes")
+        for cliente in clientes:
+            self.comboClientes.append_text(cliente[0] + " - " + cliente[1])
+        self.comboClientes.connect("changed", self.on_comboClientes_changed)
+        self.comboProductos = builder.get_object("cbListaProductos")
 
         # Botones:
         addClienteButton = builder.get_object("botonAñadir")
@@ -35,6 +43,14 @@ class FiestraPrincipal ():
         editProductoButton = builder.get_object("botonEditarProducto")
         removeProductoButton = builder.get_object("botonEliminarProducto")
         applyProductoButton = builder.get_object("botonAplicarProducto")
+
+        # Componentes bloque emergente Cliente:
+        self.nombreCliente = builder.get_object("nombreClienteText")
+        self.dniCliente = builder.get_object("dniClienteText")
+        self.fechaCliente = builder.get_object("fechaClienteText")
+        self.direccionCliente = builder.get_object("direccionClienteText")
+        self.telefonoCliente = builder.get_object("telefonoClienteText")
+        self.emailCliente = builder.get_object("emailClienteText")
 
         # Componentes bloque emergente Cliente:
         self.nombreNuevoCliente = builder.get_object("nombreNuevoCliente")
@@ -63,8 +79,27 @@ class FiestraPrincipal ():
         telefono = self.telefonoNuevoCliente.get_text()
         email = self.emailNuevoCliente.get_text()
         if dni and nombre and fecha and direccion and telefono and email:
-            self.cursor.execute("insert into clientes values ('DNI', 'NOMBRE', 'FECHA', 'DIRECCION', 45, 'EMAIL'")
+            self.cursor.execute("insert into clientes values ('" + dni + "', '" + nombre + "', '" + fecha + "', '" + direccion + "', " + telefono + ", '" + email + "')")
+            self.bbdd.commit()
+            self.newClienteBlock.set_visible(False)
+            # Se limpian los campos:
+            self.nombreNuevoCliente.set_text("")
+            self.dniNuevoCliente.set_text("")
+            self.fechaNuevoCliente.set_text("")
+            self.direccionNuevoCliente.set_text("")
+            self.telefonoNuevoCliente.set_text("")
+            self.emailNuevoCliente.set_text("")
 
+    def on_comboClientes_changed(self, combo):
+        texto = combo.get_active_text()
+        cliente = self.cursor.execute("select * from clientes where dni='" + texto.split(" - ")[0] + "'")
+        for dato in cliente:
+            self.nombreCliente.set_text(dato[0])
+            self.dniCliente.set_text(dato[1])
+            self.fechaCliente.set_text(dato[2])
+            self.direccionCliente.set_text(dato[3])
+            self.telefonoCliente.set_text(str(dato[4]))
+            self.emailCliente.set_text(dato[5])
 
     def on_addProductoButton_clicked (self, control):
         if (self.controlBloqueProducto == False):
