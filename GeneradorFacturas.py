5332c6f258262a20b6122d37c6e0ba8576a19372
Hansen <abrandarizdominguez@danielcastelao.org>
2019-03-24 19:37:20 +0100

    Can generate two types of sale report in pdf

--- GeneradorFacturas.py
+++ GeneradorFacturas.py
@@ -33,7 +33,11 @@ class FiestraPrincipal ():
 
     def on_facturaSimpleButton_clicked(self, control):
         # Se genera el contenido total de las tablas Cabezera+contenido consulta"""
-        facturaVenta = [['Nombre Producto', 'Cantidad', 'Precio Unidad', 'Precio Total']]
+        facturaVenta = [["Optica Brandariz", "", "", ""],
+                        ["CIF: 5533116B", "", "", ""],
+                        ["Direccion: Calle Falsa 123", "", "", ""],
+                        ["", "", "", ""],
+                        ['Producto', 'Cantidad', 'Precio Unidad', 'Precio Total']]
         self.cursorAux = self.bbdd.cursor()
         ventas = []
         # Para factura simple se requiere el nombre del producto, cantidad del producto, precio unidad y precio total:
@@ -53,7 +57,7 @@ class FiestraPrincipal ():
         self.i = 0
         self.sumaTotal = 0
         for registroVenta in facturaVenta:
-            if (self.i != 0):
+            if (self.i >= 5):
                 print(registroVenta)
                 self.x = 0
                 for datoVenta in registroVenta:
@@ -63,30 +67,86 @@ class FiestraPrincipal ():
             self.i = self.i + 1
         print(self.sumaTotal)
         facturaVenta.append(["", "", "", self.sumaTotal])
-
+        # Se genera el documento .pdf:
         doc = SimpleDocTemplate("FS_" + self.codigoVentaText.get_text() + "_" + registro[2] + ".pdf", pagesize=A4)
         guion = []
-
         tabla = Table(facturaVenta)
         tabla.setStyle(TableStyle(
             [
-                ('BACKGROUND', (0, 0), (-1, 0), colors.darkgreen),
-                ('TEXTCOLOR', (0, 0), (-1, 0), colors.white),
-                ('BOX', (0, 0), (-1, -1), 1, colors.black),
-                ('INNERGRID', (0, 0), (-1, -1), 1, colors.grey),
+                ('BACKGROUND', (0, 4), (-1, 4), colors.darkgreen),
+                ('BACKGROUND', (-1, -1), (-1, -1), colors.darkgreen),
+                ('TEXTCOLOR', (0, 4), (3, 4), colors.white),
+                ('TEXTCOLOR', (-1, -1), (-1, -1), colors.white),
+                ('BOX', (0, 4), (-1, -1), 1, colors.black),
+                ('INNERGRID', (0, 4), (-1, -1), 1, colors.grey),
                 ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
             ]
         ))
-
         guion.append(tabla)
-
-        """Build del documento"""
         doc.build(guion)
 
-
-
     def on_facturaDetalladaButton_clicked(self, control):
-        hola = []
+        for datoCliente in self.cursor.execute("select dni, nombre, direccion from clientes where dni in (select dnicliente from ventas where indicador='V1')"):
+            self.dni = datoCliente[0]
+            self.nombre = datoCliente[1]
+            self.direccion = datoCliente[2]
+        # Se genera el contenido total de las tablas Cabezera+contenido consulta"""
+        facturaVenta = [["Optica Brandariz", "", "", "", ""],
+                        ["CIF: 5533116B", "", "", "", ""],
+                        ["Direccion: Calle Falsa 123", "", "", "", ""],
+                        ["", "", "", "", ""],
+                        [self.nombre, "", "", "", ""],
+                        [self.dni, "", "", "", ""],
+                        [self.direccion, "", "", "", ""],
+                        ["", "", "", "", ""],
+                        ['Producto', 'Cantidad', 'Precio S/IVA', 'Precio Unidad', 'Precio Total']]
+        self.cursorAux = self.bbdd.cursor()
+        ventas = []
+        # Para factura simple se requiere el nombre del producto, cantidad del producto, precio unidad y precio total:
+        consulta = self.cursor.execute("select * from ventas where indicador='" + self.codigoVentaText.get_text() + "'")
+        for registro in consulta:
+            producto = self.cursorAux.execute(
+                "select descripcion, precio from productos where referencia='" + registro[3] + "'")
+            print(registro[3])
+            for detallesProducto in producto:
+                print("Descripcion: " + detallesProducto[0])
+                print("Precio: " + str(detallesProducto[1]))
+            print("Cantidad:" + str(registro[4]))
+            print("Precio total: " + str(detallesProducto[1] * registro[4]))
+            precioSIva = detallesProducto[1] - ((detallesProducto[1]/100)*21)
+            ventas.append([detallesProducto[0], registro[4], precioSIva, detallesProducto[1], (detallesProducto[1] * registro[4])])
+        for elemento in ventas:
+            facturaVenta.append(elemento)
+        self.i = 0
+        self.sumaTotal = 0
+        for registroVenta in facturaVenta:
+            if (self.i >= 9):
+                print(registroVenta)
+                self.x = 0
+                for datoVenta in registroVenta:
+                    if (self.x == 3):
+                        self.sumaTotal = self.sumaTotal + datoVenta
+                    self.x = self.x + 1
+            self.i = self.i + 1
+        print(self.sumaTotal)
+        facturaVenta.append(["", "", "", "", self.sumaTotal])
+        # Se genera el documento .pdf:
+        doc = SimpleDocTemplate("FD_" + self.codigoVentaText.get_text() + "_" + registro[2] + ".pdf", pagesize=A4)
+        guion = []
+        tabla = Table(facturaVenta)
+        tabla.setStyle(TableStyle(
+            [
+                ('BACKGROUND', (0, 8), (-1, 8), colors.darkgreen),
+                ('BACKGROUND', (-1, -1), (-1, -1), colors.darkgreen),
+                ('TEXTCOLOR', (0, 8), (-1, 8), colors.white),
+                ('TEXTCOLOR', (-1, -1), (-1, -1), colors.white),
+                ('BOX', (0, 8), (-1, -1), 1, colors.black),
+                ('INNERGRID', (0, 8), (-1, -1), 1, colors.grey),
+                ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+            ]
+        ))
+        guion.append(tabla)
+        doc.build(guion)
 
 if __name__ == "__main__":
     FiestraPrincipal()
